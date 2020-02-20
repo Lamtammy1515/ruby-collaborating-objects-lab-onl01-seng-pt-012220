623158f95e0177f5063b4201cdd795de0c429811
Tammy Lam <lamtammy1515@gmail.com>
2020-02-20 16:38:03 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,51 @@
+class Artist 
+  attr_accessor :name, :songs 
+  
+@@all = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+    save
+  end 
+
+    
+    def add_song(name)
+     @songs << name 
+    end
+    
+    def save 
+      @@all << self 
+    end 
+    
+    def self.all 
+      @@all 
+    end 
+    
+    def self.find_or_create_by_name(name)
+       if self.find(name) 
+         self.find(name)
+       else 
+         self.create(name)
+       end 
+    end 
+    
+    def self.create(name)
+    artist = self.new(name)
+    @@all << artist 
+    artist 
+    end
+    
+    def self.find(name)
+      @@all.find do |artist| 
+        artist.name == name 
+      end 
+    end 
+    
+    def print_songs()
+     @songs.each do |song| 
+       puts song.name
+     end 
+    end 
+    
+  end 
