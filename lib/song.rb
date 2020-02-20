6125c64b665e728e1d5b505d56d705687cfab2f2
Tammy Lam <lamtammy1515@gmail.com>
2020-02-20 16:39:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,7 +26,7 @@ class Song
   end 
   
   def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
+    new_artist = Artist.find_or_create_by_name(name)
     self.artist.add_song(name)
   end 
  
